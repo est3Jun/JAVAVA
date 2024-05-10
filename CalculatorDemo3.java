b6ad8d69a8240e7e548643fb1ced3ccb46bec4fa
est3Jun <152385475+est3Jun@users.noreply.github.com>
2024-05-10 21:59:49 +0900

    Calculator

---
+++ CalculatorDemo3.java
@@ -0,0 +1,23 @@
+class Calculator3{
+  
+    public static void sum(int left, int right){
+        System.out.println(left+right);
+    }
+     
+    public static void avg(int left, int right){
+        System.out.println((left+right)/2);
+    }
+}
+ 
+public class CalculatorDemo3 {
+     
+    public static void main(String[] args) {
+        Calculator3.sum(10, 20);
+        Calculator3.avg(10, 20);
+         
+        Calculator3.sum(20, 40);
+        Calculator3.avg(20, 40);
+    }
+ 
+}
+//합계나 평균을 구할 때만 좌항과 우항의 값을 줌.
