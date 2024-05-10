be9313fe6dc85fd22837927b9a40bab7c68211ac
est3Jun <152385475+est3Jun@users.noreply.github.com>
2024-05-10 21:58:19 +0900

    Calculator, ex_constructor

---
+++ CalculatorDemo4.java
@@ -0,0 +1,34 @@
+class Calculator{
+    int left, right;
+      
+    public void setOprands(int left, int right){
+        this.left = left;
+        this.right = right;
+    }
+      
+    public void sum(){
+        System.out.println(this.left+this.right);
+    }
+      
+    public void avg(){
+        System.out.println((this.left+this.right)/2);
+    }
+}
+  
+public class CalculatorDemo4 {
+      
+    public static void main(String[] args) {
+          
+        Calculator c1 = new Calculator();
+        //new로 인스턴스 화, c1의 데이터타입은 Calculator
+        c1.setOprands(10, 20);
+        c1.sum();       
+        c1.avg();       
+          
+        Calculator c2 = new Calculator();
+        c2.setOprands(20, 40);
+        c2.sum();       
+        c2.avg();
+    }
+  
+}
---
+++ ClaculatorDemo3.java
@@ -0,0 +1,28 @@
+public class ClaculatorDemo3 {
+ 
+    public static void avg(int left, int right) {
+        System.out.println((left + right) / 2);
+    }
+ 
+    public static void sum(int left, int right) {
+        System.out.println(left + right);
+    }
+ 
+    public static void main(String[] args) {
+        int left, right;
+ 
+        left = 10;
+        right = 20;
+ 
+        sum(left, right);
+        avg(left, right);
+ 
+        left = 20;
+        right = 40;
+ 
+        sum(left, right);
+        avg(left, right);
+    }
+ 
+}
+//입력값(left, right)을 변수화 후 sum, avg가 이 값을 사용하게 하면 코드가 줄어듬.
